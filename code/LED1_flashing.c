f0a261241bb3082958c79c4552305782b8ea2815
hiGuo <gh20222734@163.com>
2025-10-12 19:23:24 +0800

    first commit

---
+++ code/LED1_flashing.c
@@ -0,0 +1,16 @@
+#include <msp430.h>
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
+    P1DIR |= BIT0;            // set P1.0 as the output
+    while (1)
+    {
+        P1OUT |= BIT0;  // P1.0 outputs a high level
+        __delay_cycles(500000); // delay 500,000 clock cycles, approximately 0.5s
+        P1OUT &= ~BIT0;         // P1.0 outputs a low level
+        __delay_cycles(500000); // delay 500,000 clock cycles, approximately 0.5s
+    }  
+    return 0;
+    
+}
